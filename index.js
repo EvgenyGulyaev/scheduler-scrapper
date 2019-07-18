db8d12059f5e67e4d6ea88f676e906cb20e80e24
Evgeny Gulyaev <wardercompany@gmail.com>
2019-07-18 10:42:33 +0300

    Append cron

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ const { getSchedulePerWeek } = require('scheduler-ita-pretty-message'),
   audsChar = 'a',
   auds = 'HTML_AUDS',
   group = 'HTML',
-  length = 300
+  length = 300,
+  { CronJob } = require('cron')
 ;
 
 const getPromisesArray = (arrayValues, type = group, char = '') => arrayValues.map(el => getSchedulePerWeek(
   `${char}${el}`,
   type));
+
+const defaultCronCallback = (data) => {
+  console.log('data', data);
+}
 
 const getAllSchedule = async (type = all) => {
   try {
@@ -18,12 +23,12 @@ const getAllSchedule = async (type = all) => {
     if (type !== all) {
       const char = type === preps ? prepsChar : type === auds ? audsChar : '';
       const res = await Promise.all(getPromisesArray(values, type, char));
-      return res
+      return res.filter(el => el);
     }
     const prepsData = await Promise.all(getPromisesArray(values, preps, prepsChar));
     const audsData = await Promise.all(getPromisesArray(values, auds, audsChar));
     const groupData = await Promise.all(getPromisesArray(values));
-    return [...prepsData, ...audsData, ...groupData];
+    return [...prepsData, ...audsData, ...groupData].filter(el => el);
   }
   catch (e) {
     console.log('error', e)
@@ -31,11 +36,19 @@ const getAllSchedule = async (type = all) => {
   }
 }
 
-const getAllScheduleByCron = async (crontime, callback, type = all) => {
-
+const getAllScheduleByCron = async (callback = defaultCronCallback, cronTime = '0 1 * * *', type = all) => {
+  try {
+    new CronJob(cronTime, async function () {
+      const data = await getAllSchedule(type);
+      callback(data);
+    }, null, true, 'Indian/Antananarivo');
+  }
+  catch (e) {
+    console.log('error', e);
+  }
 }
 
 module.exports = {
-  getAllSchedule
+  getAllSchedule,
+  getAllScheduleByCron
 }
-
